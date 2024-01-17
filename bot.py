f0b92372b485ec7745ec86ead5848425b576f8cf
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-01-17 22:12:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,38 +17,25 @@ from pyshorteners import *
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 
-
-
-
-
-class Bot(Client):
+    class Bot(Client):
 
     def __init__(self):
         super().__init__(
-        "shortener",
-        api_id=API_ID,
-        api_hash=API_HASH,
-        bot_token=BOT_TOKEN,
-        plugins=dict(root="plugins")
+            "shortener",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            plugins=dict(root="plugins")
         )
+
     async def start(self):
-        me = await self.get_me()
-        self.owner = await self.get_users(int(OWNER_ID))
-        self.username = f'@{me.username}'
-        temp.BOT_USERNAME = me.username
-        temp.FIRST_NAME = me.first_name
-        if not await db.get_bot_stats():
-            await db.create_stats()
-        banned_users = await filter_users({"banned": True})
-        async for user in banned_users:
-            temp.BANNED_USERS.append(user["user_id"])
-        logging.info(LOG_STR)
-        await broadcast_admins(self, '** Bot started successfully **\n\nBot By @NobiDeveloper')
-        logging.info('Bot started')
-
-
-    Bot.run()
+        # your existing start method code
 
     async def stop(self, *args):
         await super().stop()
         logging.info("Bot stopped. Bye.")
+
+if __name__ == "__main__":
+    bot = Bot()
+    bot.run()
+
