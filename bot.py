6e03517623d3c8062a584a79af5bac9871ea7901
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-01-17 22:58:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,20 +32,16 @@ class Bot(Client):
     async def start(self):
     await super().start()
     me = await self.get_me()
-        self.owner = await self.get_users(int(OWNER_ID))
-        self.username = f'@{me.username}'
-        temp.BOT_USERNAME = me.username
-        temp.FIRST_NAME = me.first_name
-        if not await db.get_bot_stats():
-            await db.create_stats()
-        banned_users = await filter_users({"banned": True})
-        async for user in banned_users:
-            temp.BANNED_USERS.append(user["user_id"])
-        logging.info(LOG_STR)
-        await broadcast_admins(self, '** Bot started successfully **\n\nBot By @GreyMatter_Bots')
-        logging.info('Bot started\n\nBot By @DKBOTZ')
+    self.owner = await self.get_users(int(OWNER_ID))
+    self.username = f'@{me.username}'
+    temp.BOT_USERNAME = me.username
+    temp.FIRST_NAME = me.first_name
+    if not await db.get_bot_stats():
+        await db.create_stats()
+    banned_users = await filter_users({"banned": True})
+    async for user in banned_users:
+        temp.BANNED_USERS.append(user["user_id"])
+    logging.info(LOG_STR)
+    await broadcast_admins(self, '** Bot started successfully **\n\nBot By @GreyMatter_Bots')
+    logging.info('Bot started\n\nBot By @DKBOTZ')
 
-    async def stop(self, *args):
-        await broadcast_admins(self, '** Bot Stopped Bye **\n\nBot By @GreyMatter_Bots')
-        await super().stop()
-        logging.info('Bot Stopped Bye\n\nBot By @GreyMatter_Bots')
