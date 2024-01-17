6859b1276734e3fd4300e02de8314ac4f4bf550e
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-01-17 21:57:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,23 +32,13 @@ class Bot(Client):
         plugins=dict(root="plugins")
         )
 
-    async def start(self):
-        me = await self.get_me()
-        self.owner = await self.get_users(int(OWNER_ID))
-        self.username = f'@{me.username}'
-        temp.BOT_USERNAME = me.username
-        temp.FIRST_NAME = me.first_name
-        if not await db.get_bot_stats():
-            await db.create_stats()
-        banned_users = await filter_users({"banned": True})
-        async for user in banned_users:
-            temp.BANNED_USERS.append(user["user_id"])
-        logging.info(LOG_STR)
-        await broadcast_admins(self, '** Bot started successfully **\n\nBot By @GreyMatter_Bots')
-        logging.info('Bot started\n\nBot By @DKBOTZ')
-
-    async def stop(self, *args):
-        await broadcast_admins(self, '** Bot Stopped Bye **\n\nBot By @GreyMatter_Bots')
-        await super().stop()
-        logging.info('Bot Stopped Bye\n\nBot By @GreyMatter_Bots')
+   async def start(client, message):
+    btn = [[
+        InlineKeyboardButton('Updates Channel', url=UPDATES_CHANNEL),
+        InlineKeyboardButton('Support Group', url=SUPPORT_GROUP)
+    ],[
+        InlineKeyboardButton('Deploy', url='https://github.com/pandaznetwork/Adlinkflyshortnerbot')
+    ]]
+    text = """**Just send me link and get short link, You can also send multiple links seperated by a space or enter."""
+    await message.reply(f"👋 Hello {message.from_user.mention},\n\nI'm {WEB_NAME} Shortner bot. {text}", reply_markup=InlineKeyboardMarkup(btn))    
 
